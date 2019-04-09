a19dece659fb611950a7d796295dda370de87f79
Hao XIE <oaheix@gmail.com>
2019-04-09 19:20:41 +0800

    Chapter 07 done.

--- app/src/main/java/com/example/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/example/criminalintent/CrimeActivity.java
@@ -1,5 +1,7 @@
 package com.example.criminalintent;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,15 @@ public class CrimeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager manager = getSupportFragmentManager();
+        Fragment fragment = manager.findFragmentById(R.id.fragment_container);
+
+        if (null == fragment) {
+            fragment = new CrimeFragment();
+            manager.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
     }
 }
