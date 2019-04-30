75fbc20305abe7ff70793d74d4a20ae908db4bfd
Hao XIE <oaheix@gmail.com>
2019-04-30 12:07:40 +0800

    Chapter 11 done.

---
+++ app/src/main/java/com/example/criminalintent/CrimePagerActivity.java
@@ -0,0 +1,56 @@
+package com.example.criminalintent;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import java.util.List;
+import java.util.UUID;
+
+public class CrimePagerActivity extends AppCompatActivity {
+
+    private static final String EXTRA_CRIME_ID = "com.example.criminalintent.crime_id";
+    private ViewPager mViewPager;
+    private List<Crime> mCrimes;
+
+    public static Intent newIntent(Context context, UUID id) {
+        Intent intent = new Intent(context, CrimePagerActivity.class);
+        intent.putExtra(EXTRA_CRIME_ID, id);
+        return intent;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime_pager);
+
+        mViewPager = findViewById(R.id.activity_crime_pager_view_pager);
+
+        mCrimes = CrimeLab.getInstance(this).getCrimes();
+        FragmentManager manager = getSupportFragmentManager();
+        mViewPager.setAdapter(new FragmentStatePagerAdapter(manager) {
+            @Override
+            public Fragment getItem(int i) {
+                Crime crime = mCrimes.get(i);
+                return CrimeFragment.newInstance(crime.getId());
+            }
+
+            @Override
+            public int getCount() {
+                return mCrimes.size();
+            }
+        });
+        UUID id = (UUID) getIntent().getSerializableExtra(EXTRA_CRIME_ID);
+        int index = mCrimes.indexOf(CrimeLab.getInstance(this).getCrime(id));
+        if (index >= 0) {
+            mViewPager.setCurrentItem(index);
+        }
+    }
+}
