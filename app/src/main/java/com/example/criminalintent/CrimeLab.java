ea3a08943cf6a33804c3fe087275f944fe034d8b
Hao XIE <oaheix@gmail.com>
2019-04-30 10:16:23 +0800

    Challenge 2 of Chapter 10 done.

--- app/src/main/java/com/example/criminalintent/CrimeLab.java
+++ app/src/main/java/com/example/criminalintent/CrimeLab.java
@@ -3,21 +3,25 @@ package com.example.criminalintent;
 import android.content.Context;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.UUID;
 
-public class CrimeLab {
+class CrimeLab {
     private static CrimeLab sCrimeLab;
     private List<Crime> mCrimes;
+    private HashMap<UUID, Crime> mCrimeMap;
 
     private CrimeLab(Context context) {
         mCrimes = new ArrayList<>();
+        mCrimeMap = new HashMap<>();
         for (int i = 0; i < 100; ++i) {
             Crime crime = new Crime();
             crime.setTitle("陋习编号" + i);
             crime.setSolved(0 == i % 2);
             crime.setPoliceRequired(0 == i % 3);
             mCrimes.add(crime);
+            mCrimeMap.put(crime.getId(), crime);
         }
     }
 
@@ -32,12 +36,7 @@ public class CrimeLab {
         return mCrimes;
     }
 
-    public Crime getCrime(UUID uuid) {
-        for (Crime crime : mCrimes) {
-            if (crime.getId().equals(uuid)) {
-                return crime;
-            }
-        }
-        return null;
+    Crime getCrime(UUID uuid) {
+        return mCrimeMap.get(uuid);
     }
 }
